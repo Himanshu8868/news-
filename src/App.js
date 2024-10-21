008a7c3ca968669a41605d6aaef2902b273e59b3
Himansshu <Himanshurwt@gmail.com>
2024-10-21 11:03:55 +0530

    created Ripo and add componentDidMount for new news

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { Component } from 'react'
+import NavBar from './Componets/NavBar';
+import News from './Componets/News';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+         <News/>
+      </div>
+    )
+  }
+}
+
+export default App
+
---
+++ src/Componets/NewsItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    let {title , description , Imageurl , NewsUrl } = this.props;
+    return (
+      <div>
+        <div className="card" style={{width: "18rem"}}>
+
+  <img src={Imageurl} className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a href= {NewsUrl} target='blank' className="btn btn-primary btn-sm">Read More</a>
+  </div>
+</div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
